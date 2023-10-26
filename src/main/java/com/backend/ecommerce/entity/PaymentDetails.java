64358b3a76d8e0229d30a80843b968660a299bd9
MN-Nimit <nimitmandaviya7@gmail.com>
2023-10-26 13:25:35 +0530

    Implement Payment Integration

---
+++ src/main/java/com/backend/ecommerce/entity/PaymentDetails.java
@@ -0,0 +1,25 @@
+package com.backend.ecommerce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDetails {
+
+    private String paymentMethod;
+
+    private String status;
+
+    private String paymentId;
+
+    private String razorpayPaymentLinkId;
+
+    private String razorpayPaymentLinkReferenceId;
+
+    private String razorpayPaymentLinkStatus;
+
+    private String razorpayPaymentId;
+}
